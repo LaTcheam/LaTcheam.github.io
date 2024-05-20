6f01076c0281a11f49b67883af212e1b14e3c86a
ana571 <ana.tettoni@gmail.com>
2024-05-20 16:54:44 +0200

    tooltip
    tooltip to show year paid and name of lobby

--- visu.js
+++ visu.js
@@ -6,7 +6,7 @@
  *
  */
 
-function whenDocumentLoaded(action) {
+ function whenDocumentLoaded(action) {
 	if (document.readyState === "loading") {
 		document.addEventListener("DOMContentLoaded", action);
 	} else {
@@ -68,6 +68,35 @@ class LobbyVisu {
 				.sort((a, b) => b.wirksamkeit - a.wirksamkeit),
 		);
 
+		// Tooltip
+		const tooltip = d3.select("body")
+    		.append("div")
+   			.style("opacity", 0)
+    		.attr("class", "tooltip")
+
+		// Three function that change the tooltip when user hover / move / leave a cell
+		const mouseover = function(event, d) {
+			tooltip
+			  .transition()
+			  .duration(200)
+			  .style("opacity", 1);
+			tooltip
+				.html(`<strong>${d.data.lobby_name}</strong><br/>Années rémunérées; ${d.data.detail_geld}`)
+				.style("left", (event.pageX + 15) + "px")
+				.style("top", (event.pageY - 28) + "px")
+		  };
+		  const mousemove = function(event, d) {
+			tooltip
+			  .style("left", (event.pageX + 15) + "px")
+			  .style("top", (event.pageY - 28) + "px")
+		  };
+		  const mouseleave = function(event, d) {
+			tooltip
+			  .transition()
+			  .duration(200)
+			  .style("opacity", 0)
+		  };
+
 		// Colors
 		const style = getComputedStyle(document.body);
 		const baseStrokeColor = "#ced8ea";
@@ -89,23 +118,46 @@ class LobbyVisu {
 			.attr("fill", (d) => getColor(d.data.party, baseFillColor))
 			.attr("stroke", getStrokeColor)
 			.attr("cursor", (d) => (!d.children ? "default" : "pointer"))
-			.attr("pointer-events", (d) => (!d.children ? "none" : null))
-			.on("mouseover", function () {
-				d3.select(this)
-					.attr("stroke", selectedStrokeColor)
-					.attr("fill", selectedFillColor);
-			})
-			.on("mouseout", function () {
-				d3.select(this)
-					.attr("stroke", getStrokeColor)
-					.attr("fill", baseFillColor);
-			})
-			.on("click", (event, d) => {
-				if (focus !== d) {
-					zoom(event, d);
-					updateHierarchy(d);
-					event.stopPropagation();
+			.attr("pointer-events", null)
+			
+			.on("mouseover", function (event, d) {
+				if (d.children) {
+					// usual behaviour if children
+					d3.select(this)
+						.attr("stroke", selectedStrokeColor)
+						.attr("fill", selectedFillColor);
+				} else {
+					mouseover(event, d);
 				}
+			})
+			.on("mousemove", function(event, d) {
+				if (d.children == null) {
+					mousemove(event, d);
+				}
+			})
+			.on("mouseout", function (event, d) {
+				if (d.children) {
+					// usual behaviour if children
+					d3.select(this)
+						.attr("stroke", getStrokeColor)
+						.attr("fill", baseFillColor);
+				} else {
+					mouseleave(event, d);
+				}
+			})
+		
+			.on("click", (event, d) => {
+				if (d.children) {
+					// zoom behaviour for nodes with children
+					if (focus !== d) {
+						zoom(event, d);
+						updateHierarchy(d);
+						event.stopPropagation();
+					}
+				} else {
+					alert("leaf node clicked");
+				}
+				
 			});
 
 		// Append the text labels.
@@ -189,7 +241,6 @@ whenDocumentLoaded(() => {
 	d3.json(dataPath)
 		.then((data) => {
 			console.log("Data loaded:", data);
-
 			new LobbyVisu("circles", data);
 		})
 		.catch((error) => {
