d3b683ef458abbc1885ed5710bb11dc40a6d8eb1
Victor Schneuwly <victor.schneuwly@epfl.ch>
2024-05-30 11:06:25 +0200

    Add custom dropdown menu

---
+++ src/dropdown.js
@@ -0,0 +1,49 @@
+/**
+ * Custom dropdown component
+ *
+ * Inspiration: https://youtu.be/R4owT-LcKOo
+ */
+export class CustomDropDown {
+	constructor(id, onEvent) {
+		const dropdown = document.querySelector(id);
+		const selectOptions = dropdown.querySelector(".select-options");
+		const soValue = dropdown.querySelector("#soValue");
+		const optionSearch = dropdown.querySelector("#optionSearch");
+		const options = dropdown.querySelector(".options");
+		const optionsList = options.querySelectorAll("li");
+
+		selectOptions.addEventListener("click", () => {
+			dropdown.classList.toggle("active");
+		});
+
+		for (const li of optionsList) {
+			li.addEventListener("click", () => {
+				soValue.value = li.textContent;
+				dropdown.classList.remove("active");
+				onEvent(li.textContent);
+			});
+		}
+
+		// Filter options on input
+		optionSearch.addEventListener("input", (e) => {
+			const value = e.target.value.toLowerCase();
+			for (const li of optionsList) {
+				li.style.display = li.textContent.toLowerCase().includes(value)
+					? "block"
+					: "none";
+			}
+		});
+
+		// When pressing enter => click on first option
+		optionSearch.addEventListener("keydown", (e) => {
+			if (e.key === "Enter") {
+				for (const li of optionsList) {
+					if (li.style.display === "block") {
+						li.click();
+						break;
+					}
+				}
+			}
+		});
+	}
+}
